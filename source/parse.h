f17f7c311639bcc8810683fe8e9bd6fbcdba18fe
Dashbloxx <dashbloxx@gmail.com>
2023-09-05 20:42:20 -0400

    Implement actual enums for types

--- source/parse.h
+++ source/parse.h
@@ -2,6 +2,31 @@
 
 #include <stdbool.h>
 
+typedef enum
+{
+    TS_LONG,
+    TS_INT,
+    TS_SHORT,
+    TS_CHAR,
+    TS_BOOL
+} type_specifier_t;
+
+typedef enum
+{
+    TM_CONST,
+    TM_VOLATILE,
+    TM_RESTRICT
+} type_modifier_t;
+
+typedef enum
+{
+    SC_AUTO,
+    SC_REGISTER,
+    SC_STATIC,
+    SC_EXTERN,
+    SC_TYPEDEF
+} storage_class_t;
+
 /**
  *  @brief  This struct defines an object. All objects are to contain a label (name), and a pointer, which
  *          points to the value of the object. If it's a function, the pointer points to the start of the code
@@ -12,7 +37,7 @@ typedef struct {
     bool function;
     bool _pointer;
     void *pointer;
-    int storage_class;
-    int type_modifiers;
-    int type_specifier;
+    storage_class_t storage_class;
+    type_modifier_t type_modifiers;
+    type_specifier_t type_specifier;
 } obj_t;
